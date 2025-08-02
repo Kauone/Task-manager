c1637ba597affe9442a0b3b8d281fe4ff2d3ac9f
Kauone <raquelkauany17@gmail.com>
2025-08-01 21:09:13 -0300

    Feat: Criação da função adicionar tarefa

--- projeto/gerenciador.py
+++ projeto/gerenciador.py
@@ -1,3 +1,10 @@
+def add_task(tasks, name_task):
+    task = {"nome": name_task, "completada": False}
+    tasks.append(task)
+    print(f"Tarefa {name_task} foi adicionada com sucesso!")
+    return
+
+tasks = []
 while True:
     print("\nMenu do Gerenciador de Lista de tarefas:")
     print("1. Adicionar tarefa")
@@ -7,9 +14,12 @@ while True:
     print("5. Deletar tarefas completadas")
     print("6. Sair")
 
-    escolha = input("Digite a sua escolha: ")
+    choice = input("Digite a sua escolha: ")
 
-    if escolha == "6":
+    if choice == "1":
+        name_task = input("Digite o nome da tarefa que deseja adicionar:")
+        add_task(tasks, name_task)
+    elif choice == "6":
         break
 
 print("Programa finalizado")
